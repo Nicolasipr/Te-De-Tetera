8f7652a434d2200d248d5f3ff0b8e4e365115156
Nicolas Perez Roa <nikoxlax@gmail.com>
2019-05-24 00:17:44 -0400

    Creating add get and delete post connection
    Setting up Mongo Atlas, postman and checking if our API is working

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 require('./config/config');
+
 const express = require('express');
 const colors = require('colors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 
 const app = express();
+
+//MiddleWare
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.json()); // fixes null parser from body in postman
 
 app.use(require('./routes/index'));
+
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
 
 
 mongoose.connect(process.env.URLDB, { useCreateIndex: true, useNewUrlParser: true }, (err, res) => {
